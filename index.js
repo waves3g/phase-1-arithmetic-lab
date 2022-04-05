86e230ff6c5da342fc5c6c27920a15f9a1cf4312
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-04 21:40:20 -0700

    commit tree d6075676537a27642306a448ca0df06b777541b4

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 62;
+const num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1; random;
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+
+const max = Math.max(1,2,3,4,20)
